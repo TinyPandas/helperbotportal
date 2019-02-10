f55d93a75b268f56d4462fdfd1eae694b88a7470
TSP_Panda <chris.barwick16@gmail.com>
2019-02-09 21:38:51 -0800

    Cleanup
    Removed old stylesheet.
    Updated missed pages to new stylesheet.

--- scripts/logbuttons.js
+++ scripts/logbuttons.js
@@ -1,12 +1,6 @@
-function reqListener () {
-  console.log(this.responseText);
-}
-
 function update(src) {
-	console.log(src);
 	var doc = document.getElementById("log-embed");
 	doc.src = src;
-	console.log(doc.src);
 }
 
 function version(id) {
